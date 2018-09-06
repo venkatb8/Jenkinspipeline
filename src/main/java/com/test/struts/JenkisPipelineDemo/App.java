49b5cebcc2e44b828bff0871cf3b6bc14f4de807
nani <yarlagaddavenkatb8@gmail.com>
2018-09-05 20:57:05 -0400

    pipeline scrict

---
+++ src/main/java/com/test/struts/JenkisPipelineDemo/App.java
@@ -0,0 +1,13 @@
+package com.test.struts.JenkisPipelineDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/testPackage/DemoTesttt.java
@@ -0,0 +1,13 @@
+package testPackage;
+import org.testng.annotations.Test;
+
+public class DemoTesttt {
+	
+@Test
+public void m1(){
+	
+	System.out.println("xyz");
+	
+}
+
+}
